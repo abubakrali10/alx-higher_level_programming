ae0e3a9f2b943b8d6bdd7215d5f7016f4386f761
abubakrali10 <bakrali175@yahoo.com>
2023-05-29 16:41:48 -0700

    fixing exception message

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,7 +11,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print("division by 0")
             result = 0
         except IndexError:
-            print("division by 0")
+            print("out of range")
             result = 0
         finally:
             div_list.append(result)
