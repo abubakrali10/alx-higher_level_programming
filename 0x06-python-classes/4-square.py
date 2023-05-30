14845967f5d6b9832026d5dc3922c7df7d1e4a68
abubakrali10 <bakrali175@yahoo.com>
2023-05-30 14:48:04 -0700

    setters and getters added to Square class

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Defining a Square class"""
+
+
+class Square:
+    """Representing Sqaure class"""
+    def __init__(self, size=0):
+        """initialize square object
+        Args:
+            size: size of the new created square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """get method to retriving size"""
+        return self.__size
+
+    @property.setter
+    def size(self, value):
+        """set method to set size
+        Args:
+            value: value to set to size
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Returning Square area"""
+        return self.__size * self.__size
