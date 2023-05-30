67cf973004a0c5f61a7b00f56af335db319da593
abubakrali10 <bakrali175@yahoo.com>
2023-05-30 15:26:25 -0700

    method for printing square added

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Defining a Square class"""
+
+
+class Square:
+    """Representing Sqaure class"""
+    def __init__(self, size=0):
+        """initialize square object
+        Args:
+            size: size of the new created square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """get method to retriving size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """set method to set size
+        Args:
+            value: value to set to size
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Returning Square area"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """printing the square"""
+        if self.__size <= 0:
+            print("")
+        else:
+            for i in range(self.__size + 1):
+                for j in range(self.__size + 1):
+                    print("#", end="")
+                print("")
