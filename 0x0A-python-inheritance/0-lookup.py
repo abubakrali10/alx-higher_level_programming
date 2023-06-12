2357e5c7414e75152e15e26c2b0a333ed000d694
abubakrali10 <bakrali175@yahoo.com>
2023-06-12 13:21:39 -0700

    function return available attributes and methods in obj

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+""" ``lookup`` module.
+lookup module supplies one function, lookup(obj)
+"""
+
+
 def lookup(obj):
     """ function to get attributes and methods in obj
     Args:
