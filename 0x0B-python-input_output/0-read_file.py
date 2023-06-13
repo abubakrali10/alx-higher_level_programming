82a24354a2e7407eaeb2097658d9fd3bf69f0499
abubakrali10 <bakrali175@yahoo.com>
2023-06-13 08:10:19 -0700

    prints a content from a text file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Defines a function to read file """
+
+
+def read_file(filename=""):
+    """ prints a txt file to stdout """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
