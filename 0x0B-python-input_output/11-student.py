471e6f29f4a146c7fb1fc09fa60a34e0b6c8b609
abubakrali10 <bakrali175@yahoo.com>
2023-06-13 12:58:16 -0700

    method to replace all attributes of the Student instance

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" ``student`` module
+supplies 1 function, def to_json(self)
+"""
+
+
+class Student:
+    """ a class that defines a student """
+    def __init__(self, first_name, last_name, age):
+        """ Initializing student """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ get a dictionary description of an object """
+        my_dict = {}
+        if type(attrs) is list:
+            for i in attrs:
+                if type(i) is not str:
+                    return self.__dict__
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    my_dict[key] = value
+            return my_dict
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """ Replaces all attriubutes of student instance"""
+        for key in json:
+            self.__dict__[key] = json[key]
