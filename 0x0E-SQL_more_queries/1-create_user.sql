be0c82d4119a6f41757d4f916fac58cab2c2524b
abubakrali10 <bakrali175@yahoo.com>
2023-07-19 13:10:10 -0700

    giving user all privilege

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,7 @@
--- This script creates a user
 -- creating a user of mysql server
 CREATE USER
 	IF NOT EXISTS 'user_0d_1'@'localhost'
 	IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
 	ON *.*
-	TO 'user_0d_1'@'localhost'
-	IDENTIFIED BY 'user_0d_1_pwd';
+	TO 'user_0d_1'@'localhost';
