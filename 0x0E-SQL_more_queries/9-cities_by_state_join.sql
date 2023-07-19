5dad84d6719ebdac04a9c80dbd885c3556f8f9ae
abubakrali10 <bakrali175@yahoo.com>
2023-07-19 15:16:25 -0700

    lists all cities in hbtn_0d_usa Database

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,5 +2,5 @@
 -- lists cities by id, name and state id in asc order
 SELECT cities.id, cities.name, states.name
 FROM cities INNER JOIN states
-ON cities.state_id = cities.id
-ORDER BY cities.id
+ON cities.state_id = states.id
+ORDER BY cities.id;
