688d168f5fc588b6bb505b9f8e312f445fc5d790
abubakrali10 <bakrali175@yahoo.com>
2023-08-20 15:38:51 -0700

    list all State objects that contain the letter a from a db

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -11,7 +11,9 @@ if __name__ == "__main__":
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).filter(State.name.like("%a%"))/
-    .order_by(State.id).all()
+    states = session.query(State)\
+                    .filter(State.name.like("%a%"))\
+                    .order_by(State.id)\
+                    .all()
     for state in states:
         print(f"{state.id}: {state.name}")
