74174d9bdbe1157c391c6bd11d6f2a5c33ade765
abubakrali10 <bakrali175@yahoo.com>
2023-08-20 15:12:23 -0700

    Print first state object in a db

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -6,7 +6,7 @@ from model_state import State
 import sys
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}{}@localhost/{}"
+    engine = create_engine("mysql+mysqldb://{}{}@localhost:3306/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
