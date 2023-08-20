27acce10fc916104f7c81e0ba88784b014585b06
abubakrali10 <bakrali175@yahoo.com>
2023-08-20 15:17:45 -0700

    list all State objects from a db

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,7 +6,7 @@ from model_state import State
 import sys
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}{}@localhost/{}"
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
