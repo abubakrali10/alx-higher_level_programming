5df9afaff78a8d6e5e5bc0ff82eca14bee9492cb
abubakrali10 <bakrali175@yahoo.com>
2023-08-20 13:32:23 -0700

    script filters states that is safe from MySQL injections

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     mydb = MySQLdb.connect(host='localhost', user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3])
     cur = mydb.cursor()
-    query = ("SELECT * FROM states" +
+    query = ("SELECT * FROM states "
              "WHERE states.name = %s ORDER BY states.id")
     cur.execute(query, (sys.argv[4],))
     res = cur.fetchall()
