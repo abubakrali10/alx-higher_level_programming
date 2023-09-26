132714be6fa149452368f2fd86164459bbf172a2
abubakrali10 <bakrali175@yahoo.com>
2023-09-26 22:16:32 +0300

    get status code using request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const req = require('request');
+
+const url = process.argv[2];
+
+req(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`code: ${res.statusCode}`);
+  }
+});
